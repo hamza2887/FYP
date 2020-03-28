0f9075e3cc7c50ec546023cf5431b885e09626d0
hamza2887 <hamzajamil2887@gmail.com>
2020-03-28 12:29:56 +0500

    functionality to switches is added

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ const homeGet = (req, res) => {
 const LoginGet = (req, res) => {
   res.sendFile(__dirname + "/views/login.html");
 };
+const smokesensorsGet = (req, res) => {
+  res.sendFile(__dirname + "/views/smokesensors.html");
+};
 
 
 const data = [
@@ -40,7 +43,9 @@ const LoginPost = (req, res) => {
   }
 };
 const SensorsPost = (req, res) => {
-  console.log("Data in request....", req.body ? true : false);
+  console.log("Smoke Sensor ...." , req.body.smoke_sensor ? true : false);
+  console.log("Gas Sensor ....", req.body.gas_sensor ? true : false );
+  console.log("Motion Sensor ....", req.body.motion_sensor ? true : false );
   // res.finished();
   // res.status(200).end()
   // res.end();
@@ -51,10 +56,54 @@ const SensorsPost = (req, res) => {
   //   console.log("post data is: ", false);
   // }
 };
+
+
+const Bedroom1Post = (req, res )=> {
+  if (req.body.r1fanswitch ? true : false) {
+    console.log("BedRoom 1 Fan is ON")
+  } else {
+    console.log("BedRoom 1 Fan is OFF")
+    
+  }
+  if (req.body.r1mlightswitch ? true : false ) {
+    console.log("BedRoom 1 Main Light is ON")
+  } else {
+    console.log("BedRoom 1 Main Light is OFF")
+    
+  }
+  if (req.body.r1zlightswitch ? true : false) {
+    console.log("BedRoom 1 Zero Light is ON")
+  } else {
+    console.log("BedRoom 1 Zero Light is OFF")
+    
+  }
+  //console.log("BedRoom 1 Celing Fan ...." , req.body.r1fanswitch ? true : false);
+  //console.log("BedRoom 1 Main Light ....", req.body.r1mlightswitch ? true : false );
+  //console.log("BedRoom 1 Zero Light ....", req.body.r1zlightswitch ? true : false );
+
+};
+
+
+const Bedroom2Post = (req, res )=> {
+  console.log("BedRoom 2 Celing Fan ...." , req.body.r2fanswitch ? true : false);
+  console.log("BedRoom 2 Main Light ....", req.body.r2mlightswitch ? true : false );
+  console.log("BedRoom 2 Zero Light ....", req.body.r2zlightswitch ? true : false );
+}; 
+
+
+const KitchenPost = (req, res) => {
+  console.log("Kitchen Light ...." , req.body.klightswitch ? true : false);
+  console.log("Kitchen Exhaust fan ....", req.body.kexhaustswitch ? true : false );
+
+};
+
 app.get("/", homeGet);
-app.post("/", SensorsPost);
 app.get("/login", LoginGet);
 app.post("/login", LoginPost);
+app.post("/sensors", SensorsPost);
+app.post("/bedroom1", Bedroom1Post);
+app.post("/bedroom2", Bedroom2Post);
+app.post("/kitchen", KitchenPost);
 
 
 app.listen(port, () => console.log(`Server Started on port ${port}!`));
